b856656f78ef890567ff1a59641553eec96ecd90
Jorge <thelordjorge@gmail.com>
2020-08-03 11:46:54 -0500

    Libro.java listo Hoyos Reyna Jorge Armando

---
+++ src/java/Modelo/Libro.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author JORGE HOYOS
+ */
+public class Libro {
+    private int idlibro;
+    private String nombre;
+    private String autor;
+    private String localizacion;
+    private String estado;
+
+    public Libro() {
+    }
+
+    public int getIdlibro() {
+        return idlibro;
+    }
+
+    public void setIdlibro(int idlibro) {
+        this.idlibro = idlibro;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getLocalizacion() {
+        return localizacion;
+    }
+
+    public void setLocalizacion(String localizacion) {
+        this.localizacion = localizacion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    
+}
